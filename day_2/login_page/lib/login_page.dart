f69b8096cbe8503c6cbf61f7c8a3055c41aff626
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-08 09:15:06 +0600

    Day 2

--- day_2/login_page/lib/login_page.dart
+++ day_2/login_page/lib/login_page.dart
@@ -5,6 +5,18 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 51, 180, 255),
+      appBar: AppBar(title: Text('Login Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Sign In'),
+            Text('Registr'),
+          ],
+        ),
+      ),
+    );
   }
 }
