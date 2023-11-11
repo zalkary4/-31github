73b56450ad6e6b9585c7a2a6da41c052bc4f9696
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-11 11:25:10 +0600

    bottom sheet widg

---
+++ day_5/bottom_sheet/lib/bottom_sheet.dart.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetWidget extends StatefulWidget {
+  const BottomSheetWidget({Key? key}) : super(key: key);
+
+  @override
+  _BottomSheetWidgetState createState() => _BottomSheetWidgetState();
+}
+
+class _BottomSheetWidgetState extends State<BottomSheetWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 400,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Close'),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: Text('Modal Bottom Sheet'),
+      ),
+    );
+  }
+}
---
+++ day_5/bottom_sheet/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:bottom_sheet/bottom_sheet.dart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = true;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: isDark ? Brightness.dark : Brightness.light,
+        ),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Flutter'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                isDark = !isDark;
+                setState(() {});
+              },
+              icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode),
+            )
+          ],
+        ),
+        body: BottomSheetWidget(),
+      ),
+    );
+  }
+}
