c01525dd528dadec76261b8ecef7fba4a5908458
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-11-12 09:05:47 +0600

    Day 6

--- day_6/piano_app/lib/app.dart
+++ day_6/piano_app/lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo',
+      title: 'Demo!',
       theme: ThemeData.dark(),
       home: HomePage(),
     );
