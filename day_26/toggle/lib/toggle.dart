bf0717243036e926011601abdf097132b600940d
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-02 09:17:53 +0600

    Day 26

---
+++ day_26/toggle/lib/toggle.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ToggleButtonsWidget extends StatefulWidget {
+  const ToggleButtonsWidget({Key? key}) : super(key: key);
+
+  @override
+  _ToggleButtonsWidgetState createState() => _ToggleButtonsWidgetState();
+}
+
+class _ToggleButtonsWidgetState extends State<ToggleButtonsWidget> {
+  List<bool> isSelected = [
+    false,
+    false,
+    false,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ToggleButtons(
+        children: [
+          Icon(Icons.home),
+          Icon(Icons.settings),
+          Icon(Icons.person),
+          Icon(Icons.person),
+        ],
+        isSelected: isSelected,
+        onPressed: (int index) {
+          setState(() {
+            isSelected[index] = !isSelected[index];
+          });
+        },
+      ),
+    );
+  }
+}
