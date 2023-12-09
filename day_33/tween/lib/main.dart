552a3a5b9aa6ec510815f1bb02779f3c5ed3dc7f
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-09 19:44:05 +0600

    Day 33

---
+++ day_33/tween/lib/main.dart
@@ -0,0 +1,25 @@
+void main() {
+  Country usa = Country();
+}
+
+///void authentication (){}
+
+class Country {
+  String name;
+  int population;
+  City city;
+  List<Oblus> oblustar;
+}
+
+class City {
+  String name;
+  double area;
+  int population;
+}
+
+class Oblus {
+  String name;
+  double area;
+  List<Shaar> shaarlary;
+  Shaar borbor;
+}
