4e2a5d65b6db4a662922a0bba8186c8f3a13b240
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-10 20:58:53 +0600

    Day 34

---
+++ day_34/listenable/lib/listenable.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+final ValueNotifier<int> number = ValueNotifier(0);
+
+class ValueListenableBuilderWidget extends StatelessWidget {
+  const ValueListenableBuilderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              number.value += 1;
+            },
+            icon: Icon(Icons.add),
+          ),
+          SizedBox(height: 30),
+          ValueListenableBuilder(
+            valueListenable: number,
+            builder: (BuildContext context, int value, Widget? child) {
+              return Text(
+                '$value',
+                style: TextStyle(fontSize: 40),
+              );
+            },
+          ),
+          SizedBox(height: 50),
+          IconButton(
+            onPressed: () {
+              number.value -= 1;
+            },
+            icon: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
