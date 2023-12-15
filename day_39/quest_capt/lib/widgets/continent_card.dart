c20dc55368df53338dc7ecff5ce5c32fe4950409
Zalkary4 <zalkarbek.tilenbaev.kk@gmail.com>
2023-12-15 11:51:37 +0600

    Day 39

---
+++ day_39/quest_capt/lib/widgets/continent_card.dart
@@ -0,0 +1,41 @@
+import 'package:animation_cubs/models/continent.dart';
+import 'package:flutter/material.dart';
+
+class ContinentCard extends StatelessWidget {
+  const ContinentCard({
+    super.key,
+    required this.continent,
+  });
+
+  final Continent continent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 22,
+          child: DecoratedBox(
+            decoration: const BoxDecoration(color: Color(0xffeeeeee)),
+            child: Center(
+              child: Text(
+                continent.name,
+                style: GoogleFonts.besley(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Image.asset(
+          continent.imagePath,
+          fit: BoxFit.cover,
+        ),
+      ],
+    );
+  }
+}
